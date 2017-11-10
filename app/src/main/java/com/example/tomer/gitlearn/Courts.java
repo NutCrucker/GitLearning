44c714d3d9dadd2f1a06ca5967f27c5f547030f2
Tomer Ezon <TomerEzon@gmail.com>
2017-11-10 22:01:28 +0200

    a

--- app/src/main/java/com/example/tomer/gitlearn/Courts.java
+++ app/src/main/java/com/example/tomer/gitlearn/Courts.java
@@ -1,8 +1,55 @@
 package com.example.tomer.gitlearn;
 
-/**
- * Created by Tomer on 10/11/2017.
- */
+import android.os.AsyncTask;
+import android.os.NetworkOnMainThreadException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
-public class Courts {
+
+public class Courts extends AsyncTask<Object, Object, String> {
+    @Override
+    protected String doInBackground(Object... params) {
+        String name = params[0].toString();
+        try {
+            String check_url = "http://10.0.2.2/login.php";
+            URL url = new URL(check_url);
+            HttpURLConnection Check = (HttpURLConnection) url.openConnection();
+            Check.setRequestMethod("POST");
+            Check.setDoOutput(true);
+            Check.setDoInput(true);
+            OutputStream L = Check.getOutputStream();
+            BufferedWriter B = new BufferedWriter(new OutputStreamWriter(L, "UTF-8"));
+            String post_data = URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8");
+            B.write(post_data);
+            B.flush();
+            B.close();
+            L.close();
+            InputStream IPS = Check.getInputStream();
+            BufferedReader C = new BufferedReader(new InputStreamReader(IPS, "iso-8859-1"));
+            String result = "";
+            String line = "";
+            while ((line = C.readLine()) != null) {
+                result = result + line;
+            }
+            C.close();
+            IPS.close();
+            Check.disconnect();
+            return result;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
--- app/src/main/java/com/example/tomer/gitlearn/Kalay.java
+++ app/src/main/java/com/example/tomer/gitlearn/Kalay.java
@@ -1,13 +1,19 @@
 package com.example.tomer.gitlearn;
-
+import android.os.NetworkOnMainThreadException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class Kalay extends AppCompatActivity {
-
+    TextView A;
+    Courts C = new Courts();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalay);
-    }
+        A = (TextView)findViewById(R.id.Av);
+        A.setText((CharSequence) C.execute("kalay"));
+        }
 }
